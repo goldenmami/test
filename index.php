9e9f181b33c89b90c31823ae560284a2714b2483
goldenmami <mami.a7med@gmail.com>
2017-03-22 01:32:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
-<?php 
+<?php
 
-/*phpinfo();  */
-echo "new test! Hello world!";
-echo $_SERVER['REMOTE_HOST'];
-
-?>
+echo "Hello world from Nokia!";
+$req = $bdd->prepare('INSERT INTO commande(date, civilite, nom, prenom, societe, email, tel, service, pack, type, opt_1, opt_2, opt_3, opt_4, opt_5, precision)
+VALUES(:date, :civilite, :nom, :prenom, :societe, :email, :tel, :service, :pack, :type, :opt_1, :opt_2, :opt_3, :opt_4, :opt_5, :precision)')
+or exit(print_r($bdd->errorInfo()));
+                                   
+$req->execute(array(
+'date' => $date,
+'civilite' => $civilite,
+'nom' => $nom,
+'prenom' => $prenom,
+'societe' => $societe,
+'email' => $email,
+'tel' => $tel,
+'service' => $service,
+'pack' => $pack,
+'type' => $type,
+'opt_1' => $opt_1,
+'opt_2' => $opt_2,
+'opt_3' => $opt_3,
+'opt_4' => $opt_4,
+'opt_5' => $opt_5,
+'precision' => $precision
+));
