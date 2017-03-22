63a915c422a94925624ee7bb636aaaf4ef924ddf
goldenmami <mami.a7med@gmail.com>
2017-03-22 02:44:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include "connexion.php";
+echo "hello Nokia!";
 $str = file_get_contents('http://host-php.paas.msv-project.com/file.json/');
 $json = json_decode($str, true);
 echo '<pre>' . print_r($json, true) . '</pre>';
