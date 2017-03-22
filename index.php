ef7493d23877134d68211e8783c31d49f033baca
goldenmami <mami.a7med@gmail.com>
2017-03-22 02:40:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include "connexion.php";
-$str = file_get_contents('http://example.com/example.json/');
+$str = file_get_contents('http://host-php.paas.msv-project.com/file.json/');
 $json = json_decode($str, true);
 echo '<pre>' . print_r($json, true) . '</pre>';
 $temperatureMin = $json['daily']['data'][0]['temperatureMin'];
